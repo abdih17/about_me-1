21b1cfd3e722a08ef05a9775172b9dec6436baaf
Ben Petty <benjamin.s.petty@gmail.com>
2016-09-20 12:15:29 -0700

    scaffold html,js,css files; delete old html file

---
+++ app.js
@@ -0,0 +1,21 @@
+
+
+var username = prompt('What\'s your name?');
+alert('Welcome, ' + username + '!');
+console.log('The user\'s name is ' + username);
+document.write(username);
+
+var daddy = prompt('Who\'s your daddy?');
+alert(daddy + ' is your daddy!? Wow, ' + username + '!!');
+console.log(daddy + ' is the users daddy.');
+document.write(daddy);
+
+var rich = prompt('Is he rich like me?');
+alert('Is ' + daddy + ' rich? ' + rich + '? That\'s amazing, ' + username + '!!!');
+console.log('Is ' + daddy + ' rich? ' + rich);
+document.write(rich);
+
+var takenTime = prompt('Has he taken any time to show you what you need to live?');
+alert(takenTime + ', eh? Cool, ' + username + '. Sounds like your daddy ' + daddy + ' is really awesome because when I asked you if he was rich you said: ' + rich + '!');
+console.log('Has ' + daddy + ' taken any time to show you what you need to live: ' + takenTime);
+document.write(takenTime);

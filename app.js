95c4585505acaee3be2370d11dd20d22a0ef51dc
Ben Petty <benjamin.s.petty@gmail.com>
2016-09-21 01:34:57 -0700

    polish js,html, and css and add image to front page

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ alert('Hello! Welcome to my \"About Me\" page! Let\'s play a quick guessing game
 
 // QUESTION 1
 
-var question1 = prompt('My grandfather was a Puerto Rican CIA Field Agent who used to translate communications they intercepted of Fidel Castro for the United States government.').toLowerCase();
+var question1 = prompt('My grandfather was a Puerto Rican CIA Field Agent who used to translate intercepted communications of Fidel Castro for the United States government.').toLowerCase();
 
 while (question1 != 'yes' && question1 != 'y' && question1 != 'no' && question1 != 'n' && question1 != 'true' && question1 !='false') {
   console.log('User input invalid for Question 1: ' + question1);
   alert('Please answer either Yes, Y, No, N, True or False. \(not case sensitive\)');
-  question1 = prompt('My grandfather was a Puerto Rican CIA Field Agent who used to translate communications they intercepted of Fidel Castro for the United States government.').toLowerCase();
+  question1 = prompt('My grandfather was a Puerto Rican CIA Field Agent who used to translate intercepted communications of Fidel Castro for the United States government.').toLowerCase();
 }
 
 if (question1 === 'yes' || question1 === 'y' || question1 === 'true') {
@@ -21,67 +21,67 @@ console.log('User guessed ' + question1 + ' to Question 1');
 
 // QUESTION 2
 
-var question2 = prompt('I spend a lot of time walking around the woods in the rain looking for things to eat.').toLowerCase();
+var question2 = prompt('I spend a lot of my free time walking around the woods in the rain looking for things to eat.').toLowerCase();
 
 while (question2 != 'yes' && question2 != 'y' && question2 != 'no' && question2 != 'n' && question2 != 'true' && question2 !='false') {
   console.log('User input invalid for Question 2: ' + question2);
   alert('Please answer either Yes, Y, No, N, True or False. \(not case sensitive\)');
-  question2 = prompt('I spend a lot of time walking around the woods in the rain looking for things to eat.').toLowerCase();
+  question2 = prompt('I spend a lot of my free time walking around the woods in the rain looking for things to eat.').toLowerCase();
 }
 
 if (question2 === 'yes' || question2 === 'y' || question2 === 'true') {
-  alert('YEP! It\'s true. I love foraging for mushrooms around my top secret sweet spots in the Pacific NW \(No, I won\'t tell you the exact location\). I also love digging for clams and scooping up oysters off the beach \(and eating them raw/alive\)');
+  alert('YEP! It\'s true. I love foraging for fungus in the forest every Fall. I also love digging clams and scooping up oysters off the beach \(and eating them raw/alive\)');
 } else if (question2 === 'no' || question2 === 'n' || question2 === 'false') {
-  alert('Actually, it\'s true. I love foraging for mushrooms around my top secret sweet spots in the Pacific NW \(No, I won\'t tell you the exact location\). I also love digging for clams and scooping up oysters off the beach \(and eating them raw/alive\)');
+  alert('Wrong! I love foraging for fungus in the forest every Fall. I also love digging clams and scooping up oysters off the beach \(and eating them raw/alive\)');
 };
 console.log('User guessed ' + question2 + ' to Question 2');
 
 // QUESTION 3
 
-var question3 = prompt('I never graduated from high school and did not receive my GED until I was 31 years old.').toLowerCase();
+var question3 = prompt('I dropped out of high school in the 10th grade to start my own business selling screenprinted T-shirts and Sweatshirts.').toLowerCase();
 
 while (question3 != 'yes' && question3 != 'y' && question3 != 'no' && question3 != 'n' && question3 != 'true' && question3 !='false') {
   console.log('User input invalid for Question 3: ' + question3);
   alert('Please answer either Yes, Y, No, N, True or False. \(not case sensitive\)');
-  question3 = prompt('I never graduated from High School and did not receive my GED until I was 31 years old.').toLowerCase();
+  question3 = prompt('I dropped out of high school in the 10th grade to start my own business selling screenprinted T-shirts and Sweatshirts.').toLowerCase();
 }
 
 if (question3 === 'yes' || question3 === 'y' || question3 === 'true') {
-  alert('It\'s true, I dropped out of high school in the 10th grade to start my own business selling screen-printed t-shirts and sweatshirts. I supported myself on that income for over 15 years.');
+  alert('Yes! It\'s true, and I supported myself on that income for over 15 years.');
 } else if (question3 === 'no' || question3 === 'n' || question3 === 'false') {
-  alert('Actually, it\'s true. I dropped out of high school in the 10th grade to start my own business selling screen-printed t-shirts and sweatshirts. I supported myself on that income for over 15 years.');
+  alert('Nope, I totally did that! ...and I supported myself on that income for over 15 years!');
 };
 console.log('User guessed ' + question3 + ' to Question 3');
 
 // QUESTION 4
 
-var question4 = prompt('My uncles legally hunted and killed a grey whale in 1999 on a hand carved canoe with harpoons and a 50 cal rifle, after fasting, bathing in ice water and covering their bare skin in stinging nettles.').toLowerCase();
+var question4 = prompt('My uncles legally hunted and killed a gray whale in 1999 on a hand carved canoe with harpoons and a .577 T-rex, after fasting, bathing in ice water and covering their bare skin in stinging nettles.').toLowerCase();
 
 while (question4 != 'yes' && question4 != 'y' && question4 != 'no' && question4 != 'n' && question4 != 'true' && question4 !='false') {
   console.log('User input invalid for Question 4: ' + question4);
   alert('Please answer either Yes, Y, No, N, True or False. \(not case sensitive\)');
-  question4 = prompt('My uncles legally hunted and killed a grey whale in 1999 on a hand carved canoe with harpoons and a 50 cal rifle, after fasting, bathing in ice water and covering their bare skin in stinging nettles.').toLowerCase();
+  question4 = prompt('My uncles legally hunted and killed a gray whale in 1999 on a hand carved canoe with harpoons and a .577 T-rex, after fasting, bathing in ice water and covering their bare skin in stinging nettles.').toLowerCase();
 }
 
 if (question4 === 'yes' || question4 === 'y' || question4 === 'true') {
-  alert('100% true. The Makah tribe was granted whaling rights in our treaty with the US government in 1855. The discipline and training required by the tribe in order to participate in these hunts is extremely difficult.');
+  alert('Correct! The Makah tribe was granted whaling rights in our treaty with the US government in 1855, and my uncles succesfully harvested one on May 17, 1999. The discipline and training required by the tribe in order to participate in these hunts is extremely difficult.');
 } else if (question4 === 'no' || question4 === 'n' || question4 === 'false') {
-  alert('That statement was 100% true. The Makah tribe was granted whaling rights in our treaty with the US government in 1855. The discipline and training required by the tribe in order to participate in these hunts is extremely difficult.');
+  alert('That statement was actually true, believe it or not. The Makah tribe was granted whaling rights in our treaty with the US government in 1855, and my uncles succesfully harvested one on May 17, 1999. The discipline and training required by the tribe in order to participate in these hunts is extremely difficult.');
 };
 console.log('User guessed ' + question4 + ' to Question 4');
 
 // QUESTION 5
 
-var question5 = prompt('I\'ve never missed a Dave Mathews Band concert at the Gorge.').toLowerCase();
+var question5 = prompt('I\'ve never missed a Dave Mathews Band concert at the Gorge, in all of my adult life.').toLowerCase();
 
 while (question5 != 'yes' && question5 != 'y' && question5 != 'no' && question5 != 'n' && question5 != 'true' && question5 !='false') {
   console.log('User input invalid for Question 5: ' + question5);
   alert('Please answer either Yes, Y, No, N, True or False. \(not case sensitive\)');
-  question5 = prompt('I\'ve never missed a Dave Mathews Band concert at the Gorge.').toLowerCase();
+  question5 = prompt('I\'ve never missed a Dave Mathews Band concert at the Gorge, in all of my adult life.').toLowerCase();
 }
 
 if (question5 === 'yes' || question5 === 'y' || question5 === 'true') {
-  alert('Wrong. I\'ve never been to a Dave Mathews Band concert, and I probably never will.');
+  alert('Wrong! I\'ve never been to a Dave Mathews Band concert, and I probably never will.');
 } else if (question5 === 'no' || question5 === 'n' || question5 === 'false') {
   alert('Correct. I\'ve never been to a Dave Mathews Band concert, and I probably never will.');
 };
